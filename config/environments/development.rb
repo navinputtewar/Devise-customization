40e4242cf4dce5142c03aac8fd06c58473dbe47c
navinputtewar <newd31@gmail.com>
2015-07-14 19:38:31 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'example.com',
-    user_name:            'newd31@gmail.com',
-    password:             '9370754342',
+    user_name:            '',
+    password:             '',
     authentication:       'plain',
     enable_starttls_auto: true  }
 
