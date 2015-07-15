17e1f241aa06091a1c386411e98de10769236457
navinputtewar <newd31@gmail.com>
2015-07-15 17:01:48 +0530

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'newd31@gmail.com',
+    password:             '9175792918',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
